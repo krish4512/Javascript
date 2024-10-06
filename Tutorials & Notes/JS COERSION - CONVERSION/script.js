e60fc6f2e8307bc7b60a63358f22b8751b151160
Krish Patel <krish76.kp@gmail.com>
2024-10-05 23:40:52 -0400

    Added Values Comparision and Coersion

---
+++ Tutorials & Notes/JS COERSION - CONVERSION/script.js
@@ -0,0 +1,8 @@
+let a = 1
+let b = '8'
+b= parseInt (b,10) //converting the variable to number with parsing
+
+
+let c= a+b //add
+
+console.log('Answer :'+ c); //print
---
+++ Tutorials & Notes/JS Value Comparision Operator/script.js
@@ -0,0 +1,18 @@
+let PI = "3.14" // = is for assignment 
+
+if(PI ==3.14)  //checks datatype Only 
+{
+    console.log("This is PI");
+}
+else{
+    console.log("This is Not PI");
+}
+
+
+if(PI ==3.14) //checks datatype + Value
+{
+    console.log("This is PI");
+}
+else{
+    console.log("This is Not PI");
+}
